56d94f75d7e94a08473c4d348fcf1b8812ee812f
Unknown <danila.vpopov@gmail.com>
2022-01-22 14:06:29 -0800

    current location card

--- scripting/script.js
+++ scripting/script.js
@@ -5,20 +5,32 @@ const api = "871b1ca27dfbb558cfcae80665059258";
 const iconImg = document.getElementById("weather-icon");
 const loc = document.querySelector("#location");
 const tempC = document.querySelector(".c");
-const desc = document.querySelector(".desc");
+const desc = document.querySelector(".weatherDescription");
+const uviT = document.querySelector(".uvi");
 
 window.addEventListener("load", () => {
   let long;
   let lat;
-  // give me your location prompt
+  // Accesing Geolocation of User
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       // Storing Longitude and Latitude in variables
       long = position.coords.longitude;
       lat = position.coords.latitude;
       const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+      const uviD = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,daily&appid=${api}`;
 
       // Using fetch to get data
+      fetch(uviD)
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((data) => {
+          const { uvi } = data.current;
+          uviT.textContent = `UV index: ${uvi}`;
+        });
+
       fetch(base)
         .then((response) => {
           return response.json();
@@ -27,7 +39,7 @@ window.addEventListener("load", () => {
           const { temp } = data.main;
           const place = data.name;
           const { description, icon } = data.weather[0];
-          const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+          const iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
 
           // Interacting with DOM to show data
           iconImg.src = iconUrl;
