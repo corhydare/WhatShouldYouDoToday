775ab531b400c2fbd9e06b9d88197baa9d38c1dd
Unknown <danila.vpopov@gmail.com>
2022-01-22 21:42:02 -0800

    current weather cleaned up

--- scripting/script.js
+++ scripting/script.js
@@ -1,26 +1,26 @@
-// personal key goes into api constant
-
+// personal key goes into api constant (it will be disabled after the graded project, for security)
 const api = "871b1ca27dfbb558cfcae80665059258";
 
-const iconImg = document.getElementById("weather-icon");
+const iconP = document.getElementById("icon-present");
 const loc = document.querySelector("#location");
-const tempC = document.querySelector(".c");
+const temps = document.querySelector(".temperature");
 const desc = document.querySelector(".weatherDescription");
 const uviT = document.querySelector(".uvi");
 
 window.addEventListener("load", () => {
-  let long;
+  let lon;
   let lat;
-  // Accesing Geolocation of User
+  // geolocation prompt
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      // Storing Longitude and Latitude in variables
-      long = position.coords.longitude;
+      // storing Longitude and Latitude in variables
+      lon = position.coords.longitude;
       lat = position.coords.latitude;
-      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
-      const uviD = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,daily&appid=${api}`;
+      // normal weather and uv index are in separate api's now
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}&units=metric`;
+      const uviD = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${api}`;
 
-      // Using fetch to get data
+      // using fetch to get UVI data
       fetch(uviD)
         .then((response) => {
           console.log(response);
@@ -30,7 +30,7 @@ window.addEventListener("load", () => {
           const { uvi } = data.current;
           uviT.textContent = `UV index: ${uvi}`;
         });
-
+      // ucing fetch to get other weather values
       fetch(base)
         .then((response) => {
           return response.json();
@@ -39,13 +39,15 @@ window.addEventListener("load", () => {
           const { temp } = data.main;
           const place = data.name;
           const { description, icon } = data.weather[0];
+          // @2x.png icon is twice the size but it doesn't have shading
           const iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
 
-          // Interacting with DOM to show data
-          iconImg.src = iconUrl;
+          // changing text in html doc
+          iconP.src = iconUrl;
           loc.textContent = `${place}`;
+          // Weather description in words, for possible accessibility
           desc.textContent = `${description}`;
-          tempC.textContent = `${temp.toFixed(2)} °C`;
+          temps.textContent = `${temp.toFixed(2)} °C`;
         });
     });
   }
